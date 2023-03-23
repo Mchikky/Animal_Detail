0850cfd389d7a6f06e0a590684645fe12e645115
Chikaodili Deng <mchikky@yahoo.com>
2023-03-23 14:30:58 +0100

    Initial Commit

---
+++ Animal_Detail/Animal_DetailApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animal_DetailApp.swift
+//  Animal_Detail
+//
+//  Created by Chikaodili Deng on 23/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Animal_DetailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
